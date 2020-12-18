80b75d1074a4b0dd83da9514912cbae51e22f9ed
Marcelo de Souza Dias <marcelosouzadias@hotmail.com>
2020-12-18 20:09:58 -0300

    Atualizando o curso

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,8 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send("Rota de Atendimento"))
+    app.get('/atendimentos', (req, res) => res.send("Você esta na roda de atendimento realizando um GET"))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send("Você esta na roda de atendimento realizando um post")
+    })
 }
