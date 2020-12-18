3f1ae85c637c2e31367384f2acc75813694b317c
Marcelo de Souza Dias <marcelosouzadias@hotmail.com>
2020-12-18 19:00:58 -0300

    Atualizando o curso

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const express = require('express')
+const customExpress = require('./config/customExpress')
 
-const app = express()
-
+const app = customExpress()
 app.listen(3000, () => console.log('Servidor Rodando na porta 3000'))
-
-app.get('/', (req, res) => res.send("Servidor Rodando na porta 3000"))
