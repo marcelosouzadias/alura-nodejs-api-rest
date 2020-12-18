d9d0337cbcce39e6dde7b4131ed374d94888feb5
unknown <marcelo@marcelo.magna.intra.net>
2020-12-18 18:39:34 -0300

    inicio do curto

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log('Servidor Rodando na porta 3000'))
+
+app.get('/', (req, res) => res.send("Servidor Rodando na porta 3000"))
